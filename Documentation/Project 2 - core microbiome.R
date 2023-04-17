4053bafe8237d8cd554ea1d2c25df190ee91dc84
ma-aj <124547626+ma-aj@users.noreply.github.com>
2023-04-17 13:27:30 -0700

    uploaded figures and updated core microbiome script

--- Documentation/Project 2 - core microbiome.R
+++ Documentation/Project 2 - core microbiome.R
@@ -22,6 +22,7 @@ crp_abun_6m <- transform_sample_counts(crp_infant_6m_final, fun=function(x) x/su
 #12 months
 agp_abun_12m <- transform_sample_counts(agp_infant_12m_final, fun=function(x) x/sum(x))
 crp_abun_12m <- transform_sample_counts(crp_infant_12m_final, fun=function(x) x/sum(x))
+
 #Subsetting 
 #6 months
 agp_6m_above <- subset_samples(agp_abun_6m, agp_clin=="Above")
@@ -37,47 +38,47 @@ crp_12m_below <- subset_samples(crp_abun_12m, crp_median=="Below")
 
 #Core microbiome for Venn Diagram
 #6 months
-core_agp_6m_above <- core_members(agp_6m_above, detection=0, prevalence=0.7)
-core_agp_6m_below <- core_members(agp_6m_below, detection=0, prevalence=0.7)
-core_crp_6m_above <- core_members(crp_6m_above, detection=0, prevalence=0.7)
-core_crp_6m_below <- core_members(crp_6m_below, detection=0, prevalence=0.7)
+core_agp_6m_above <- core_members(agp_6m_above, detection=0, prevalence=0.8)
+core_agp_6m_below <- core_members(agp_6m_below, detection=0, prevalence=0.8)
+core_crp_6m_above <- core_members(crp_6m_above, detection=0, prevalence=0.8)
+core_crp_6m_below <- core_members(crp_6m_below, detection=0, prevalence=0.8)
   
 #12 months
-core_agp_12m_above <- core_members(agp_12m_above, detection=0, prevalence=0.7)
-core_agp_12m_below <- core_members(agp_12m_below, detection=0, prevalence=0.7)
-core_crp_12m_above <- core_members(crp_12m_above, detection=0, prevalence=0.7)
-core_crp_12m_below <- core_members(crp_12m_below, detection=0, prevalence=0.7)
+core_agp_12m_above <- core_members(agp_12m_above, detection=0, prevalence=0.8)
+core_agp_12m_below <- core_members(agp_12m_below, detection=0, prevalence=0.8)
+core_crp_12m_above <- core_members(crp_12m_above, detection=0, prevalence=0.8)
+core_crp_12m_below <- core_members(crp_12m_below, detection=0, prevalence=0.8)
 
 #Visualization
 
 #4-way 6 months 
 four_way_6m <- ggVennDiagram(x=list(core_agp_6m_above, core_agp_6m_below, core_crp_6m_above, core_crp_6m_below), category.names = c("High AGP","Low AGP","High CRP", "Low CRP"), legend.position = "right", label = "count", label_size = 4)
-core_6m <- four_way_6m_count + scale_x_continuous(expand = expansion(mult = .2)) + labs(caption = "6-month-old infants", fill = "# Genera") + theme(plot.caption = element_text(size = 12, hjust=0.5), plot.margin = margin(r = 20)) + scale_fill_gradient(low = "#EBE8FC", high = "#756BB1") + border() 
+core_6m <- four_way_6m + scale_x_continuous(expand = expansion(mult = .2)) + labs(caption = "6-month-old infants", fill = "# Genera") + theme(plot.caption = element_text(size = 12, hjust=0.5), plot.margin = margin(r = 20)) + scale_fill_gradient(low = "#EBE8FC", high = "#756BB1") + border() 
 core_6m
-ggsave("four_way_6m.png", plot = last_plot(), height = 3.5, width = 5, dpi = 300)
+#ggsave("four_way_6m(0.80prev).png", plot = last_plot(), height = 3.5, width = 5, dpi = 300)
 
 #4-way 12 month (count)
 four_way_12m_count <- ggVennDiagram(x=list(core_agp_12m_above, core_agp_12m_below, core_crp_12m_above, core_crp_12m_below), category.names = c("High AGP","Low AGP","High CRP", "Low CRP"), legend.position = "right", label = "count", label_size = 4)
 core_12m <- four_way_12m_count + scale_x_continuous(expand = expansion(mult = .2)) + labs(caption = "12-month-old infants", fill = "# Genera") + theme(plot.caption = element_text(size = 12, hjust=0.5), plot.margin = margin(r = 20)) + scale_fill_gradient(low = "#EBE8FC", high = "#756BB1") + border()
 core_12m
-ggsave("four_way_12m.png", plot = last_plot(), height = 3.5, width = 5, dpi = 300)
+#ggsave("four_way_12m(0.80prev).png", plot = last_plot(), height = 3.5, width = 5, dpi = 300)
 
 ggarrange(core_6m, core_12m, ncol = 2, nrow = 1) + bgcolor("white")
 
-ggsave("combinedcore_plot.png", plot = last_plot(), height = 7, width = 15, dpi = 300)
+#ggsave("combinedcore_plot(0.80prev).png", plot = last_plot(), height = 7, width = 15, dpi = 300)
 
 #Core phyloseq
 #6 months
-core_highagp_6m <- core(agp_6m_above, detection=0, prevalence=0.7)
-core_lowagp_6m <- core(agp_6m_below, detection=0, prevalence=0.7)
-core_highcrp_6m <- core(crp_6m_above, detection=0, prevalence=0.7)
-core_lowcrp_6m <- core(crp_6m_below, detection=0, prevalence=0.7)
+core_highagp_6m <- core(agp_6m_above, detection=0, prevalence=0.8)
+core_lowagp_6m <- core(agp_6m_below, detection=0, prevalence=0.8)
+core_highcrp_6m <- core(crp_6m_above, detection=0, prevalence=0.8)
+core_lowcrp_6m <- core(crp_6m_below, detection=0, prevalence=0.8)
 
 #12 months
-core_highagp_12m <- core(agp_12m_above, detection=0, prevalence=0.7)
-core_lowagp_12m <- core(agp_12m_below, detection=0, prevalence=0.7)
-core_highcrp_12m <- core(crp_12m_above, detection=0, prevalence=0.7)
-core_lowcrp_12m <- core(crp_12m_below, detection=0, prevalence=0.7)
+core_highagp_12m <- core(agp_12m_above, detection=0, prevalence=0.8)
+core_lowagp_12m <- core(agp_12m_below, detection=0, prevalence=0.8)
+core_highcrp_12m <- core(crp_12m_above, detection=0, prevalence=0.8)
+core_lowcrp_12m <- core(crp_12m_below, detection=0, prevalence=0.8)
 
 #Identifying Core Taxa
 #6 months
